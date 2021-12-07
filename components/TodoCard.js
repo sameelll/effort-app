c7e3eb5e464f79b1f58a496cc9e3341515eec0dd
Eren Akel <eren.akel.46@gmail.com>
2021-12-07 20:32:18 +0300

    Card components created, styling has some problems

--- components/TodoCard.js
+++ components/TodoCard.js
@@ -7,7 +7,7 @@ function TodoCard({ duty }) {
     const { task, title } = duty;
 
     return (
-        <div className="border-l-2 mt-10">
+        
             <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-blue-600 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
 
                 <div className="w-5 h-5 bg-blue-600 absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
@@ -25,7 +25,6 @@ function TodoCard({ duty }) {
                     <CheckIcon className="button w-15 h-15" />
                 </div>
             </div>
-        </div>
     )
 }
 
